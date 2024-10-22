fbb1bd2b57f7e436731d30e2fdf1a0a277c71cc1
alanaleticia04 <169085788+alanaleticia04@users.noreply.github.com>
2024-10-22 17:55:17 -0300

    Add files via upload

--- cadastra-leitores.php
+++ cadastra-leitores.php
@@ -1,35 +1,44 @@
 <?php
 include "config.php";
 if (!$conn){
-    die("falha na conexão {mysqli_connect_error(){");
-}
-// recebe os dados do formulário
-$nome = 'teste nome';
-$dtnasc = '2007-02-19';
-$celular ='81';
-$email = 'allaninha2018@gmail.com';
-$ra = '000000';
-$endereco = 'rua josé reginaldo dos santos';
-$num_end = '55';
-$bairro = 'jardim paulista baixo';
-$cidade = 'paulista ';
+die ("Falha na conexão {mysqli_connect_error()}");
 
-//cria variavel para inserir o registro
-$sql = "INSERT INTO `leitores`(`Nome`, `DtNasc`, `Celular`, `Email`, `RA`, `Endereco`, `NumEnd`, `Bairro`, `CidadeUF`) values 
-('$nome', '$dtnasc', '$celular', '$email', '$ra', '$endereco', '$num_end', '$bairro', '$cidade')";
+} 
+//receber os dados do formulário
+$nome = 'Bianca';
+$dtnasc = '2007-02-04';
+$celular = '(81) 9990-4508';
+$email = 'siqueirabianca254@gmail.com';
+$ra = '356788';
+$endereco = 'Torres';
+$num_end = '46'; 
+$bairro = 'Janga';
+$cidade = 'Paulista/PE';
 
-// executa a consulta SQL. Se falhar, exibe o erro do banco de dados 
-$query = mysqli_query(mysql: $conn,query: $sql) or
-die(mysqli_error(mysql: $db));
+// cria variável para inserir o registro
+$sql = "INSERT INTO `leitores`
+( `Nome`, `DtNasc`, `Celular`, `Email`, `RA`, `Endereco`, `NumEnd`, `Bairro`, `CidadeUF`) VALUES
+ ('$nome','$dtnasc','$celular','$email','$ra','$endereco','$num_end','$bairro','$cidade')";
 
-if($query){
-    echo "<center>";
-    echo "cadastro realizado com sucesso!!<br>";
-    echo "<a href= 'index.php'><button title='home page'>voltar</button></a>";
-    echo "</center>";
-} else {
-    echo "<center>";
-    echo "<a href= 'index.php'><button title='home page'>voltar</button></a>";
-    echo "</center>";
-}
+ //executa a consulta SQL. Se falhar, exibe o erro do banco de dados
+ $query= mysqli_query (mysql:$conn,query: $sql)  or
+ die (mysqli_error (mysql:$conn));
+
+ if ($query){
+echo "<center>";
+echo "Cadastro realizado com sucesso!!<br>";
+echo " <a href = 'index.php'><button title = 'Home page'>Voltar</button></a>";
+echo "</center>";
+ }else {
+echo "<center>";
+echo" <a href = 'index.php'><button title = 'Home page'>Voltar</button></a>";
+echo "</center>";
+
+ }
 ?>
+
+
+
+ 
+
+
